9da87358d51d93c6b2612db816fd66f047090740
vhnakai <hugo.nakai@gmail.com>
2020-06-25 10:00:51 -0300

    Setting Add & Remove Handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from "react";
+import api from './services/api';
 
 import "./styles.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+
+  const [repositories , setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((res)=> {
+      setRepositories(res.data);
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const res =   await api.post("repositories", {
+      id,
+      title,
+      url,
+      techs
+    });
+
+    const repository = res.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
